cc2e52194d545c5260b9f6b19583f0694ffddbc8
Nikita Kozlov <74086640+kinitaleino@users.noreply.github.com>
2020-11-14 21:21:14 +0500

    Add files via upload

---
+++ hometast1.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+protocol Quadrangle{
+
+    var a:Int {get set}
+    var b:Int {get set}
+ 
+}
+
+class Figure{
+
+    var name:String = "Figure"
+    var cornerRadius:Int = 90
+
+}
+
+
+
+class Triangle:Figure{
+
+   override init() {
+       super.init()
+       self.name = "Triangle"
+       self.cornerRadius = 60
+   }
+}
+
+class Rectangle:Figure{
+
+   override init() {
+       super.init()
+       self.name = "Rectangle"
+       self.cornerRadius = 90
+   }
+
+}
+
+class Round:Figure{
+
+   override init() {
+       super.init()
+       self.name = "Round"
+       self.cornerRadius = 360
+   }
+
+}
